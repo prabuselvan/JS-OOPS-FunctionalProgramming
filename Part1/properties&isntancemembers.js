f3a7d5b980cc2400f2e6a196cd7d5c0dc5350d4f
Prabu Elamuruguselvan <prabuselvan@Prabus-MacBook-Pro.local>
2019-01-31 23:29:21 +0530

    properties and instancemembers

---
+++ Part1/properties&isntancemembers.js
@@ -0,0 +1,32 @@
+function Circle (radius) {
+    // instance members
+    this.radius=radius;
+    this.draw= function () {
+        console.log('draw');
+        // accessing the prototype members from instance members
+        this.shape();
+    }
+}
+
+
+// console.log(c1);
+// Circle.prototype we can change the implementation directly on the function constrcutor
+// Prototype members
+Circle.prototype.shape= function() {
+    console.log('shape');
+    // accessing the instance members from prototype members
+    // this.draw();
+}
+// console.log(c1.__proto__===Circle.prototype);
+
+Circle.prototype.toString = function () {
+    return 'Circle with radius ' + this.radius
+};
+
+const c1= new Circle(122);
+
+const c2= new Circle(500);
+
+// console.log(c1.toString());
+// console.log(c2.toString());
+c1.draw();
